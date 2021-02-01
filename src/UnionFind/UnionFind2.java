b83f65bc2d2f1f7862069cc62553192626cb23a2
Jaden Neal <longweicai1997@163.com>
2021-02-01 19:32:02 +0800

    update Trie & UnionFind

---
+++ src/UnionFind/UnionFind2.java
@@ -0,0 +1,44 @@
+package UnionFind;
+
+public class UnionFind2 implements UF {
+    private int[] parent;
+
+    public UnionFind2(int size){
+        parent = new int[size];
+        for (int i =0; i < size; i ++)
+            parent[i] = i;
+    }
+
+    @Override
+    public int getSize() {
+        return parent.length;
+    }
+
+    // 查看元素p所对应的集合编号
+    // 时间复杂度：O(h)
+    private int find(int p){
+        if (p < 0 || p >= parent.length)
+            throw new IllegalArgumentException("p is out of bound.");
+        while (p != parent[p])
+            p = parent[p];
+        return p;
+    }
+
+
+    // 查看元素p和元素q是否属于同一集合
+    @Override
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    @Override
+    public void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if (pRoot == qRoot)
+            return;
+        parent[pRoot] = qRoot;
+
+    }
+}
